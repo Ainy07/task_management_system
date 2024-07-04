eb35b17bb43c13d1eb1f58652fca7443a56df9f0
Ainy Gupta <111982864+Ainy07@users.noreply.github.com>
2024-07-04 22:29:35 +0530

    task

---
+++ task_management_system/tasks/views.py
@@ -0,0 +1,24 @@
+from rest_framework import viewsets
+from .models import Task, Label
+from .serializers import TaskSerializer, LabelSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+class LabelViewSet(viewsets.ModelViewSet):
+    serializer_class = LabelSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Label.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
